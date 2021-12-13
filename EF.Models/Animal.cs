792d41dceee1040806929f4b0f19aeb483f3fc2a
shohag06 <shohag.cse06@gmail.com>
2021-12-13 10:50:39 -0800

    ClassAdded

---
+++ EF.Models/Animal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EF.Models
+{
+    class Animal
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Food { get; set; }
+        public string Origin { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ EF.Models/EfContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EF.Models
+{
+    class EfContext:DbContext
+    {
+        public EfContext():base("EfContext")
+        {
+
+        }
+    }
+}
